23f9630d8c55a0a1f7119c4602bfd3c791e7a77d
John Tsevdos <tsevdosjohn@gmail.com>
2021-01-14 18:12:25 +0200

    ex 1 solution

--- intermediate/workshop/todo-app/src/TodoApp.js
+++ intermediate/workshop/todo-app/src/TodoApp.js
@@ -2,14 +2,32 @@ import React, { useState } from "react";
 import TodoItem from "./components/TodoItem";
 
 const TodoApp = ({ initialTodos }) => {
+  const [toDoInput, setToDoInput] = useState("");
   const [todos, setTodos] = useState(initialTodos);
+
+  const updateInput = (e) => {
+    setToDoInput(e.target.value);
+  };
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      title: toDoInput,
+      done: false,
+    };
+
+    setTodos([...todos, newTodo]);
+    setToDoInput("");
+  };
 
   return (
     <div>
       <h2>My ToDos</h2>
       <form>
-        Add ToDo: <input type="text" />
-        <button type="submit">Add ToDo</button>
+        Add ToDo: <input type="text" value={toDoInput} onChange={updateInput} />
+        <button type="submit" onClick={addTodo}>
+          Add ToDo
+        </button>
       </form>
       <ul>
         {todos.map((todo, index) => (
